9b9971219492ce3873c0f313af3163f1d0ffcdba
Danyal Moazzam <danyalmoazzamjan@gmail.com>
2024-03-30 02:03:34 +0500

    CLI ATM MACHINE

---
+++ index.js
@@ -0,0 +1,95 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Initializing user balance and pin
+let myBalance = 5000;
+let myPin = 2233;
+// Print welcome message
+console.log(chalk.blue.bold.underline("\n \tWelcome to Danyal - ATM Machine\n"));
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: chalk.yellow.bold("\nEnter your pin: ")
+    }
+]);
+// Take input from user
+if (pinAnswer.pin === myPin) {
+    console.log(chalk.greenBright("\nYour pin is correct.Logged In Successfully\n"));
+    // Take input from user to select operation method.
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: chalk.yellow.bold("\nWhat do you want to do?\n"),
+            choices: ["Withdraw", "Deposit", "Check Balance", "Exit"]
+        }
+    ]);
+    // Withdrawel Method.
+    if (operationAns.operation === "Withdraw") {
+        let withdrawAns = await inquirer.prompt([
+            {
+                name: "withdrawMethod",
+                type: "list",
+                message: chalk.yellow.bold("\nSelect a Withdrawel Method.\n"),
+                choices: ["Fast Cash", "Enter Amount"]
+            }
+        ]);
+        if (withdrawAns.withdrawMethod === "Fast Cash") {
+            let amountAns = await inquirer.prompt([
+                {
+                    name: "fastCash",
+                    type: "list",
+                    message: chalk.yellow.bold("\nSelect amount to withdraw: \n"),
+                    choices: ["500", "1000", "2000", "5000", "10000", "20000"]
+                }
+            ]);
+            if (amountAns.fastCash > myBalance) {
+                console.log("\nYou don't have enough balance. Please try again.\n");
+            }
+            else {
+                myBalance -= amountAns.fastCash;
+                console.log(chalk.green.bold("\nWithDraw Successful.\nYour New balance is: \n" + myBalance));
+                console.log("\nThank you for using Danyal - ATM\n");
+            }
+        }
+        if (withdrawAns.withdrawMethod === "Enter Amount") {
+            let amountAns = await inquirer.prompt([
+                {
+                    name: "amount",
+                    type: "number",
+                    message: chalk.yellow.bold("\nEnter amount to withdraw: \n")
+                }
+            ]);
+            if (amountAns.amount > myBalance) {
+                console.log("\nYou don't have enough balance.\n");
+            }
+            else {
+                myBalance -= amountAns.amount;
+                console.log(chalk.green.bold("WithDraw Successful.Your New balance is: " + myBalance));
+                console.log("\nThank you for using Danyal - ATM\n");
+            }
+        }
+    }
+    else if (operationAns.operation === "Deposit") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: chalk.yellow.bold("\nEnter amount to deposit: \n")
+            }
+        ]);
+        myBalance += amountAns.amount;
+        console.log("Deposit Successful.\nYour New balance is: " + myBalance);
+        console.log("\nThank you for using Danyal - ATM\n");
+    }
+    else if (operationAns.operation === "Check Balance") {
+        console.log(chalk.green.bold("\nYour balance is: " + myBalance));
+        console.log("\nThank you for using Danyal - ATM\n");
+    }
+    else if (operationAns.operation === "Exit") {
+        console.log(chalk.green.bold("\nThank you for using Danyal - ATM\n"));
+    }
+}
+else if (pinAnswer.pin !== myPin) {
+    console.log(chalk.red.bold.underline("Pin is incorrect.\n Try Again!"));
+}
